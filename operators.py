1cac51f3ecdd5ea07bbeb70e8225c53aa2c0c567
opeyemi olawuyi <fresky4reala@yahoo.com>
2023-03-22 17:09:53 +0100

    my code

---
+++ operators.py
@@ -0,0 +1,16 @@
+names = ["andrew", "mario", "david",]
+
+schools = ("uniabuja", "uniben", "unilag")
+
+place = "gwarinpa junction"
+
+food ={"rice", "beans", "spag"}
+
+print("john" in names)
+
+print("e" in place)
+
+print("rice" in food)
+
+print("unibadan" in schools)
+
