c9dd365303b2245199f0d7f7e4a271deab50b463
opeyemi olawuyi <fresky4reala@yahoo.com>
2023-03-22 17:03:01 +0100

    my code

---
+++ access.py
@@ -0,0 +1,19 @@
+my_list = [1, 2, 3, 10, 30, 10]
+print(len(my_list)) #find length of list
+print(my_list.index(10)) #find index of element that occurs first
+print(my_list.count(10)) #find count of the element
+print(sorted(my_list))#print sorted list but not change original
+my_list.sort(reverse=True) #sort original list
+print(my_list)
+my_dict = {} #empty dictionary
+print(my_dict)
+my_dict = dict() #empty dictionary
+print(my_dict)
+my_dict = {1: 'python', 2:'Java'} #dictionary with elements
+print(my_dict)
+my_tuple = (1,2,3)
+my_tuple1 = my_tuple + (4,5,6) #add elements
+print(my_tuple1)
+print(my_tuple)
+my_dict = {}
+print(type(my_dict))
